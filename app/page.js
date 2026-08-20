242a636c01927a9bddb004fa95c047f4a5c07be9
surfrajahmad91 <surfrajahmad@gmail.com>
2026-08-20 23:43:00 +0530

    Add Home component with booking information

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>VOYNU</h1>
+      <p>Book your ride. Travel with ease.</p>
+
+      <button>Book a Cab</button>
+    </main>
+  );
+}
