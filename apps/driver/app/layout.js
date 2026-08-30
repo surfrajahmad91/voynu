d03488388456770b35786c1329b4eef0c84840d7
surfrajahmad91 <surfrajahmad@gmail.com>
2026-08-30 18:11:45 +0530

    Add VOYNU Saarthi app shell

---
+++ apps/driver/app/layout.js
@@ -0,0 +1,5 @@
+export const metadata = { title: "VOYNU Saarthi", description: "VOYNU Saarthi app" };
+
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
