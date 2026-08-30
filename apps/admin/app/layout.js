90c0c72b666c2f64c8b60d10af9cd443ea0ffe63
surfrajahmad91 <surfrajahmad@gmail.com>
2026-08-30 18:11:56 +0530

    Add VOYNU Admin app shell

---
+++ apps/admin/app/layout.js
@@ -0,0 +1,5 @@
+export const metadata = { title: "VOYNU Admin", description: "VOYNU administration" };
+
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
