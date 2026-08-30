14c40bff8f0d24912ed8b23a1eac8d2e550cbeb2
surfrajahmad91 <surfrajahmad@gmail.com>
2026-08-30 18:11:35 +0530

    Add VOYNU web app shell

---
+++ apps/web/app/layout.js
@@ -0,0 +1,5 @@
+export const metadata = { title: "VOYNU", description: "VOYNU cab booking" };
+
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
