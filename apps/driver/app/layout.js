056b177b08248ba6992eae6bfb5938c6fdc59e7b
surfrajahmad91 <surfrajahmad@gmail.com>
2026-08-31 22:29:52 +0530

    Remove global 10-second refresh from Saarthi

--- apps/driver/app/layout.js
+++ apps/driver/app/layout.js
@@ -1,5 +1,4 @@
 import "../styles/globals.css";
-import LiveRefresh from "../components/LiveRefresh";
 
 export const metadata = { title: "VOYNU Saarthi", description: "VOYNU Saarthi driver app" };
 
@@ -8,7 +7,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         {children}
-        <LiveRefresh />
         <script dangerouslySetInnerHTML={{__html:`if("serviceWorker"in navigator){window.addEventListener("load",()=>navigator.serviceWorker.register("/sw.js").catch(()=>{}));}`}} />
       </body>
     </html>
