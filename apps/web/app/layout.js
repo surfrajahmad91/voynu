673df249bfc9216b9a36ea5c1b6814bc33a5d035
surfrajahmad91 <surfrajahmad@gmail.com>
2026-09-02 00:48:35 +0530

    cleanup: remove stale apps/web project

--- apps/web/app/layout.js
+++
@@ -1,7 +0,0 @@
-import "../styles/globals.css";
-
-export const metadata = { title: "VOYNU", description: "Book your perfect trip with VOYNU" };
-
-export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}</body></html>;
-}
